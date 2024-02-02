9ce835d607006960eed281f900b11166c2bde36d
issei1213 <59830008+issei1213@users.noreply.github.com>
2024-02-02 17:08:40 +0900

    TODO管理用のコンポーネントを作成

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,10 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Link,
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
 
 export default function App() {
@@ -15,10 +16,21 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+      <body style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/todos">TODO一覧</Link></li>
+                    <li><Link to="/todos/aaa">TODO詳細</Link></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <Outlet />
+            <ScrollRestoration />
+            <Scripts />
+        </main>
+
       </body>
     </html>
   );
